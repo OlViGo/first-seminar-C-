b7a8f5ffadfd0ab8fdb5efe3fd53c1eb857fd7df
OlViGo <olgabibendum@gmail.com>
2022-10-04 10:28:54 +0300

    Task 1,Task2,Task3,Task4, gitignore

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите задачу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом
+//  второго  
+// a= 25  b= 5 ->да
+// a=2 b =10 -> нет
+// a=9 b= -3 ->да
+// а=-3 b = 9-> нет
+
+
+Console.Write ("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine());
+
+if (numberOne == numberTwo * numberTwo)
+{
+Console.WriteLine ("Да");
+}
+else
+{
+Console.WriteLine ("Нет");   
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int numberWeek = int.Parse(Console.ReadLine());
+
+if (numberWeek  == 1)
+{
+Console.WriteLine( "Monday");
+}
+if (numberWeek  == 2)
+{
+Console.WriteLine( "Tuesday");    
+}
+if (numberWeek  == 3)
+{
+Console.WriteLine( "Wednesday");   
+}
+if (numberWeek  == 4)
+{
+Console.WriteLine( "Thursday");   
+}   
+if (numberWeek  == 5)
+{
+ Console.WriteLine( "Friday");     
+}
+if (numberWeek  == 6)
+{
+Console.WriteLine( "Saturday"); 
+}
+if (numberWeek  == 7)
+{
+Console.WriteLine( "Sunday");     
+}
+
